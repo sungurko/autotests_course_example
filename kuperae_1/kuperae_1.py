8a4f0680800f93874a3eacc8a27db4cfc665868e
kuperae <sungurko@mail.ru>
2023-05-01 15:37:59 +0300

    #1_commit

---
+++ kuperae_1/kuperae_1.py
@@ -0,0 +1,8 @@
+first_name = 'Александр'
+second_name = 'Сунгуров'
+years = 11
+job_title = 'Инженер-Технолог'
+
+print('Меня зовут', first_name, second_name)
+print('Стаж работы компании Тензор составляет', years, 'лет')
+print('Должность', job_title)
