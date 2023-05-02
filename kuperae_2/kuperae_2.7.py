d23db87369e37673a53762cbfaa2a9d5da3a97c1
kuperae <sungurko@mail.ru>
2023-05-02 16:26:47 +0300

    #9_commit

---
+++ kuperae_2/kuperae_2.7.py
@@ -0,0 +1,18 @@
+# Дана 2 строки из неповторяющихся символов
+first_string = 'wtf'
+second_string = 'brick quz jmpy veldt whangs fox'
+
+# Найдем индексы первого вхождения символа первой строки во вторую
+w = second_string.find(first_string[0])
+t = second_string.find(first_string[1])
+f = second_string.find(first_string[2])
+
+# Найдем минимальный и максимальный индекс
+start = min (w, t, f )
+stop = max (w, t, f )
+
+# Сделаем срез по минимальному и максимальному индексу
+result = second_string[start:stop + 1]
+
+# Выведем на печать результат
+print('Срез минимальной длины:', result)
