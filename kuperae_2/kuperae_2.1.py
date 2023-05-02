0c77e6584e0cc1c3fc41006598b45619b09c6feb
kuperae <sungurko@mail.ru>
2023-05-02 10:59:04 +0300

    #2_commit

---
+++ kuperae_2/kuperae_2.1.py
@@ -0,0 +1,9 @@
+side_square = 72
+
+perimeter_square = 4 * side_square
+area_square = side_square ** 2
+diagonal_square = side_square * (2 ** 0.5)
+print('Периметр квадрата равен:', perimeter_square,
+      '\nПлощадь квадрата равна:', area_square,
+      '\nДиагональ квадрата равна:', diagonal_square)
+
